8b0d33953ea47062f3b4c6ce20cd5a51a8e97f62
Sychevskyi Henadii <henadii.sychevskyi@virtuozzo.com>
2026-01-14 15:56:43 +0100

    JE-75278

--- scripts/add_admin_user.py
+++ scripts/add_admin_user.py
@@ -17,14 +17,18 @@ def main() -> None:
     with registry.cursor() as cr:
         env = api.Environment(cr, SUPERUSER_ID, {})
         users = env["res.users"]
-        admin = users.create(
-            {
-                "name": "Administrator",
-                "login": admin_user,
-                "email": admin_email,
-            }
-        )
-        admin.write({"password": admin_pswd})
+        admin = users.search([("login", "=", admin_user)], limit=1)
+        if admin:
+            admin.write({"password": admin_pswd, "email": admin_email})
+        else:
+            admin = users.create(
+                {
+                    "name": "Administrator",
+                    "login": admin_user,
+                    "email": admin_email,
+                    "password": admin_pswd,
+                }
+            )
         cr.commit()
 
 
