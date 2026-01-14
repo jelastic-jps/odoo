e89eb82dd91ae9ac7f63ddfb09735768254c2cae
Sychevskyi Henadii <henadii.sychevskyi@virtuozzo.com>
2026-01-14 15:47:31 +0100

    JE-75278

--- scripts/add_admin_user.py
+++ scripts/add_admin_user.py
@@ -6,28 +6,26 @@ from odoo import api, SUPERUSER_ID
 def main() -> None:
     db_name = os.environ["ODOO_DB_NAME"]
     odoo_conf = os.environ["ODOO_CONFIG"]
-    master_pswd = os.environ["ODOO_MASTER_PSWD"]
-    admin_email = os.environ.get("ODOO_ADMIN_EMAIL", "admin@example.com")
+    admin_pswd = os.environ["ODOO_ADMIN_PSWD"]
+    admin_user = os.environ["ODOO_ADMIN_USER"]
+    admin_email = os.environ["ODOO_ADMIN_EMAIL"]
 
+    # Load server configuration
     odoo.tools.config.parse_config(["-c", odoo_conf])
 
-    with odoo.api.Environment.manage():
-        registry = odoo.registry(db_name)
-        with registry.cursor() as cr:
-            env = api.Environment(cr, SUPERUSER_ID, {})
-            users = env["res.users"]
-            admin = users.search([("login", "=", "admin")], limit=1)
-            if not admin:
-                admin = users.create(
-                    {
-                        "name": "Administrator",
-                        "login": "admin",
-                        "email": admin_email,
-                    }
-                )
-            if not admin.email:
-                admin.email = admin_email
-            admin._set_password(master_pswd)
+    registry = odoo.registry(db_name)
+    with registry.cursor() as cr:
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        users = env["res.users"]
+        admin = users.create(
+            {
+                "name": "Administrator",
+                "login": admin_user,
+                "email": admin_email,
+            }
+        )
+        admin.write({"password": admin_pswd})
+        cr.commit()
 
 
 if __name__ == "__main__":
